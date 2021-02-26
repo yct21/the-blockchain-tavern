11422e1b758ad1e136a6a8cee5bb8bf6df4ea5ba
Chutian Yang <yct21@12tcy.com>
2021-02-26 12:45:59 +0800

    Write CLI matches

---
+++ cli/tbt/src/balances.rs
@@ -0,0 +1,3 @@
+pub(crate) fn list_balances() {
+
+}
--- cli/tbt/src/main.rs
+++ cli/tbt/src/main.rs
@@ -1,3 +1,51 @@
+// TODO:
+// check balance
+// add tx
+
+mod balances;
+
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("The Blockchain Tarvern")
+        .author("Chutian Yang <yct21@12tcy.com>")
+        .about("The Blockchain Tarvern CLI")
+        .subcommand(
+            SubCommand::with_name("balances")
+                .about("Interact with balances")
+                .subcommand(SubCommand::with_name("list").about("List all balances")),
+        )
+        .subcommand(
+            SubCommand::with_name("tx")
+                .about("Interact with transaction")
+                .subcommand(
+                    SubCommand::with_name("add")
+                        .about("Add new tx to database")
+                        .arg(
+                            Arg::with_name("from")
+                                .required(true)
+                                .takes_value(true)
+                                .help("From what account to send tokens"),
+                        )
+                        .arg(
+                            Arg::with_name("to")
+                                .required(true)
+                                .takes_value(true)
+                                .help("To what account to send tokens"),
+                        )
+                        .arg(
+                            Arg::with_name("value")
+                                .required(true)
+                                .takes_value(true)
+                                .help("How many tokens to send"),
+                        )
+                        .arg(
+                            Arg::with_name("data")
+                                .takes_value(true)
+                                .possible_values(&["reward"])
+                                .help("Data of transaction"),
+                        ),
+                ),
+        )
+        .get_matches();
 }
